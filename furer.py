9ca400b6ad4c07122badbb43330c1842c71e7554
hamsinikadali <76052347+hamsinikadali@users.noreply.github.com>
2023-11-20 08:42:42 +0530

    Update furer.py

--- furer.py
+++ furer.py
@@ -1,17 +1,23 @@
 def furer_multiply(x, y):
+    
+    #checking size of number
     if x < 2**64 or y < 2**64:
         return x * y
-
+        
+    #Determining the size of the numbers and dividing them into halves
     m = min(x.bit_length(), y.bit_length())
     m2 = m // 2
 
+    # Creating a mask to extract the lower m/2 bits
     mask = (1 << m2) - 1
 
+    # Spliting the numbers into four parts
     a = x >> m2
     b = x & mask
     c = y >> m2
     d = y & mask
 
+    # Recursively computing multiplications
     ac = furer_multiply(a, c)
     bd = furer_multiply(b, d)
     ad_bc = furer_multiply(a + b, c + d) - ac - bd
