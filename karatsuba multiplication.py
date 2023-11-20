7b450c84e18ea5afd672d6deda10871ba76e1864
hamsinikadali <76052347+hamsinikadali@users.noreply.github.com>
2023-11-20 22:22:32 +0530

    Update karatsuba multiplication.py

--- karatsuba multiplication.py
+++ karatsuba multiplication.py
@@ -1,14 +1,14 @@
+import time
 def karatsuba_multiplication(x, y):
-    """Multiply two integers using Karatsuba's algorithm."""
     # Base case for recursion
     if x < 10 or y < 10:
         return x * y
 
-    # Calculate the size of the numbers
+    # Calculating the size of the numbers
     n = max(len(str(x)), len(str(y)))
     half = n // 2
 
-    # Split the digit sequences about the middle
+    # Spliting the digit sequences about the middle
     high1, low1 = x // 10**half, x % 10**half
     high2, low2 = y // 10**half, y % 10**half
 
@@ -20,25 +20,16 @@ def karatsuba_multiplication(x, y):
     return (z2 * 10 ** (2 * half)) + ((z1 - z2 - z0) * 10 ** half) + z0
 
 def main():
-    # Accept user input
-
         # Here we are assuming the user will input valid integers
         x = int(input("Enter the first number to multiply: "))
         y = int(input("Enter the second number to multiply: "))
-
+        start_time = time.time()
         result = karatsuba_multiplication(x, y)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"Execution Time: {execution_time} seconds")
         print(f"Result = {result}")
 
 
-# The script starts here
 if __name__ == "__main__":
     main()
-
-import time
-
-start_time = time.time()
-# Run your algorithm here
-end_time = time.time()
-
-execution_time = end_time - start_time
-print(f"Execution Time: {execution_time} seconds")
