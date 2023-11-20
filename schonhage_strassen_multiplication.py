469817aa265e53a0aeabb94e7f70b505b10cb7ca
hamsinikadali <76052347+hamsinikadali@users.noreply.github.com>
2023-11-20 22:33:45 +0530

    Update schonhage_strassen_multiplication.py

--- schonhage_strassen_multiplication.py
+++ schonhage_strassen_multiplication.py
@@ -1,10 +1,8 @@
 import numpy as np
-
+import time
 
 class GFG:
     a, b = 0, 0
-
-    @staticmethod
     def schonhage_strassen_multiplication():
         a_digits = GFG.get_digits(GFG.a)
         b_digits = GFG.get_digits(GFG.b)
@@ -21,7 +19,6 @@ class GFG:
         product = int(''.join(map(str, result)))
         print("\nThe Product is:", product)
 
-    @staticmethod
     def get_digits(num):
         # Converting the number to a list of digits
         return [int(digit) for digit in str(num)]
@@ -30,5 +27,9 @@ class GFG:
 if __name__ == "__main__":
     GFG.a = 114701722186227725530857903247788731566117812708912875331083418612132595800326
     GFG.b = 987643221443489764384795643298746382985736482736487365847365874387458437564327
-
+    start_time = time.time()
     GFG.schonhage_strassen_multiplication()
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Execution Time: {execution_time} seconds")
+
